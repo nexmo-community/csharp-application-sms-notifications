d50f1f2c20fd84f82d816f94c140157ffd7c9689
Joydip Kanjilal <joydipkanjilal@users.noreply.github.com>
2021-07-23 00:34:12 +0530

    Startup class updated

--- NotificationsUsingVonage/Startup.cs
+++ NotificationsUsingVonage/Startup.cs
@@ -23,6 +23,7 @@ namespace NotificationsUsingVonage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<NotificationManager>();
             services.AddQuartz(typeof(NotificationJob));
             services.AddControllersWithViews();
         }
