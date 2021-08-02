da47686443685512369b4adbef30df451da80fdf
Joydip Kanjilal <joydipkanjilal@users.noreply.github.com>
2021-08-02 15:36:37 +0530

    Checked in the solution file

---
+++ NotificationsUsingVonage - 101/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace NotificationsUsingVonage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
